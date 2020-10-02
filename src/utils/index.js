d377f4eeb22653d975f69050cd7c552c3b3826bd
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2020-10-02 12:58:58 -0700

    docs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,14 @@
 /**
  * Calculate points
  *  - 4 letter words = 1 pt
- *  - Every letter after is +1 pt
+ *  - 5 letter and biggers words are 1pt for each letter
  *  - If all 7 letters are used ("pangram"), +7pts
  *
  * @param  {[String]} array of words (or single word)
  * @return {Number} points
  */
 export const calcPoints = (words) => words.reduce((totalPoints, word) => {
-  let wordPoints = word.length - 3;
+  let wordPoints = (word.length === 4) ? 1 : word.length;
   if (isPangram(word)) {
     wordPoints += 7;
   }
